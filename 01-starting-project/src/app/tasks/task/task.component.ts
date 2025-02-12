901c27a7e29861282ec9a041b14fe37ccee5df8d
Diogo Pereira <diogo.pereira@ezoqc.com>
2025-02-12 15:26:24 -0500

    angular essentials

--- 01-starting-project/src/app/tasks/task/task.component.ts
+++ 01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,10 +1,13 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Task } from './task.model';
+import { CardComponent } from "../../shared/card/card.component";
+import { DatePipe } from '@angular/common';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
@@ -12,9 +15,9 @@ import { Task } from './task.model';
 
 export class TaskComponent {
   @Input() task!: Task;
-  @Output() complete = new EventEmitter<string>(); 
+  private tasksservice = inject(TasksService)
 
   onCompleteTask(){
-    this.complete.emit(this.task.id);
+    this.tasksservice.removeTask(this.task.id);
   }
 }
