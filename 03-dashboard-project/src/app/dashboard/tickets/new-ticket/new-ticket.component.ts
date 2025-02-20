ee5d958cec4cf38e56071ac13b2cffdb597ff0ea
Diogo Pereira <diogo.pereira@ezoqc.com>
2025-02-20 17:20:34 -0500

    tickets component

--- 03-dashboard-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 03-dashboard-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  add = output<{title: string, text: string}>()
+  enteredTitle = ""
+  enteredText = ""
 
+  onSubmit(){
+    this.add.emit({title: this.enteredTitle, text: this.enteredText})
+
+    this.enteredTitle = "";
+    this.enteredText = "";
+  }
 }
