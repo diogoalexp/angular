ed2546f081b4fa45084e84ccd58e817eb9731b4b
Diogo Pereira <diogo.pereira@ezoqc.com>
2025-03-08 09:18:09 -0500

    add and update tasks with service

--- 07-services/src/app/tasks/new-task/new-task.component.ts
+++ 07-services/src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, viewChild } from '@angular/core';
+import { Component, ElementRef, inject, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TaskService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -11,7 +12,10 @@ import { FormsModule } from '@angular/forms';
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
 
+  private taskService = inject(TaskService)
+
   onAddTask(title: string, description: string) {
+    this.taskService.addTask({title, description})
     this.formEl()?.nativeElement.reset();
   }
 }
