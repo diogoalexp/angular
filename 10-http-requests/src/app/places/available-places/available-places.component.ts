251153020de9be8d6d4876996ad6994dbb0bafd3
Diogo Pereira <diogo.pereira@ezoqc.com>
2025-03-29 11:05:54 -0400

    fecth data from api

--- 10-http-requests/src/app/places/available-places/available-places.component.ts
+++ 10-http-requests/src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +13,40 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+  isFetching = signal(false);
+  error = signal('');
+
+  ngOnInit(): void {
+    this.isFetching.set(true);
+    const subscription = this.httpClient.get<{places: Place[]}>("http://localhost:3000/places", {
+      observe: 'response'
+    })
+    .pipe(
+      map((response) => response.body),
+      catchError((error) => {
+        console.log(error);
+        return throwError(() => new Error("Fail to fetch available places"));
+      })
+    )
+    .subscribe({
+      next: (data) =>{
+        console.log(data)
+        this.places.set(data?.places)
+      },
+      error: (error: Error) => {
+        this.error.set(error.message)
+      },
+      complete: () =>{
+        this.isFetching.set(false);
+      }
+    });
+
+    this.destroyRef.onDestroy(() =>{
+      subscription.unsubscribe();
+    })
+  }
 }
