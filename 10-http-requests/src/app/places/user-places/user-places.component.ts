46537f6adb0198c1286c2482f2f8c0513ad6d0b7
Diogo Pereira <diogo.pereira@ezoqc.com>
2025-03-29 11:17:02 -0400

    load user places

--- 10-http-requests/src/app/places/user-places/user-places.component.ts
+++ 10-http-requests/src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +13,40 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit{
+    places = signal<Place[] | undefined>(undefined);
+    private httpClient = inject(HttpClient);
+    private destroyRef = inject(DestroyRef);
+    isFetching = signal(false);
+    error = signal('');
+
+    ngOnInit(): void {
+      this.isFetching.set(true);
+      const subscription = this.httpClient.get<{places: Place[]}>("http://localhost:3000/user-places", {
+        observe: 'response'
+      })
+      .pipe(
+        map((response) => response.body),
+        catchError((error) => {
+          console.log(error);
+          return throwError(() => new Error("Fail to fetch your favorite places"));
+        })
+      )
+      .subscribe({
+        next: (data) =>{
+          console.log(data)
+          this.places.set(data?.places)
+        },
+        error: (error: Error) => {
+          this.error.set(error.message)
+        },
+        complete: () =>{
+          this.isFetching.set(false);
+        }
+      });
+
+      this.destroyRef.onDestroy(() =>{
+        subscription.unsubscribe();
+      })
+    }
 }
