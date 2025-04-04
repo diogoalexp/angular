f4d51220bbd1941ff414b2563bbfc3494b7c8118
Diogo Pereira <diogo.pereira@ezoqc.com>
2025-04-04 07:38:15 -0400

    nested routes

--- 12-routes/app.routes.ts
+++ 12-routes/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { TaskComponent } from "./src/app/tasks/task/task.component";
 import { NoTaskComponent } from "./src/app/tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./src/app/users/user-tasks/user-tasks.component";
+import { NewTaskComponent } from "./src/app/tasks/new-task/new-task.component";
 
 export const routes: Routes = [
     {
@@ -10,7 +11,17 @@ export const routes: Routes = [
     },
     {
         path: 'users/:userId', //<your-domain>/users/<uid>
-        component: UserTasksComponent
+        component: UserTasksComponent,
+        children: [
+            {
+                path: 'tasks', //<your-domain>/users/<uid>tasks
+                component: TaskComponent
+            },
+            {
+                path: 'tasks/new', //<your-domain>/users/<uid>tasks
+                component: NewTaskComponent
+            }
+        ]
     },
     {
         path: 'tasks', //<your-domain>/tasks
