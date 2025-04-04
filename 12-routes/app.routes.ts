c75387bcdd60db454f866731e3f612d361aa48f3
Diogo Pereira <diogo.pereira@ezoqc.com>
2025-04-04 08:15:00 -0400

    spliting routes file

--- 12-routes/app.routes.ts
+++ 12-routes/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from "@angular/router";
-import { TaskComponent } from "./src/app/tasks/task/task.component";
+import { routes as userRoutes } from './src/app/users/users.routes'
 import { NoTaskComponent } from "./src/app/tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./src/app/users/user-tasks/user-tasks.component";
-import { NewTaskComponent } from "./src/app/tasks/new-task/new-task.component";
-import { TasksComponent } from "./src/app/tasks/tasks.component";
 import { NotFoundComponent } from "./src/app/not-found/not-found.component";
 
 export const routes: Routes = [
@@ -14,16 +12,7 @@ export const routes: Routes = [
     {
         path: 'users/:userId', //<your-domain>/users/<uid>
         component: UserTasksComponent,
-        children: [
-            {
-                path: 'tasks', //<your-domain>/users/<uid>tasks
-                component: TasksComponent
-            },
-            {
-                path: 'tasks/new', //<your-domain>/users/<uid>tasks
-                component: NewTaskComponent
-            }
-        ]
+        children: userRoutes
     },
     {
         path: '**', //not match other paths
