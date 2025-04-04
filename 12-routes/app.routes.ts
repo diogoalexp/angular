9aa66ab3b025da6e753f648e5ed7b23c903b05ea
Diogo Pereira <diogo.pereira@ezoqc.com>
2025-04-04 07:55:11 -0400

    params in child routes

--- 12-routes/app.routes.ts
+++ 12-routes/app.routes.ts
@@ -3,6 +3,7 @@ import { TaskComponent } from "./src/app/tasks/task/task.component";
 import { NoTaskComponent } from "./src/app/tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./src/app/users/user-tasks/user-tasks.component";
 import { NewTaskComponent } from "./src/app/tasks/new-task/new-task.component";
+import { TasksComponent } from "./src/app/tasks/tasks.component";
 
 export const routes: Routes = [
     {
@@ -15,7 +16,7 @@ export const routes: Routes = [
         children: [
             {
                 path: 'tasks', //<your-domain>/users/<uid>tasks
-                component: TaskComponent
+                component: TasksComponent
             },
             {
                 path: 'tasks/new', //<your-domain>/users/<uid>tasks
