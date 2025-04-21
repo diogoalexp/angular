fd8ff61b84aad79e117b782e985d902ed833c406
Diogo Pereira <diogo.pereira@ezoqc.com>
2025-04-21 09:08:36 -0400

    introducing selectors

--- 14-ngrx-store/src/app/counter-output/counter-output.component.ts
+++ 14-ngrx-store/src/app/counter-output/counter-output.component.ts
@@ -3,6 +3,7 @@ import { Observable, Subscription } from 'rxjs';
 
 import { Store } from '@ngrx/store';
 import { AsyncPipe } from '@angular/common';
+import { selectCount, selectDoubleCount } from '../store/counter.selector';
 
 @Component({
   selector: 'app-counter-output',
@@ -16,8 +17,11 @@ export class CounterOutputComponent implements OnInit, OnDestroy {
   // counterServiceSub?: Subscription;
 
   count$: Observable<number>;
+  doubleCount$: Observable<number>;
+
   constructor (private store : Store<{counter: number}>){
-    this.count$ = this.store.select('counter');
+    this.count$ = this.store.select(selectCount);
+    this.doubleCount$ = this.store.select(selectDoubleCount);
     this.count$.subscribe();
   }
 
