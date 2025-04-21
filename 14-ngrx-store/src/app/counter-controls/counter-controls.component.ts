abf3cc8ada90e42fa0753c6862ad9e71996cfd02
Diogo Pereira <diogo.pereira@ezoqc.com>
2025-04-21 08:54:06 -0400

    creating a reducer and action

--- 14-ngrx-store/src/app/counter-controls/counter-controls.component.ts
+++ 14-ngrx-store/src/app/counter-controls/counter-controls.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
-import { CounterService } from '../counter.service';
+import { Store } from '@ngrx/store';
+import { increment, IncrementAction } from '../store/counter.actions';
 
 @Component({
   selector: 'app-counter-controls',
@@ -9,13 +9,14 @@ import { CounterService } from '../counter.service';
   standalone: true,
 })
 export class CounterControlsComponent {
-  constructor(private counterService: CounterService) {}
+  constructor(private store: Store) {}
 
   increment() {
-    this.counterService.increment();
+    // this.store.dispatch(increment({value: 2}));
+    this.store.dispatch(new IncrementAction(2));
   }
 
   decrement() {
-    this.counterService.decrement();
+    // this.counterService.decrement();
   }
 }
--- 14-ngrx-store/src/app/counter-output/counter-output.component.ts
+++ 14-ngrx-store/src/app/counter-output/counter-output.component.ts
@@ -1,29 +1,37 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
-import { CounterService } from '../counter.service';
+import { Store } from '@ngrx/store';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css'],
   standalone: true,
+  imports: [AsyncPipe]
 })
 export class CounterOutputComponent implements OnInit, OnDestroy {
-  counter = 0;
-  counterServiceSub?: Subscription;
+  // counter = 0;
+  // counterServiceSub?: Subscription;
 
-  constructor(private counterService: CounterService) {}
+  count$: Observable<number>;
+  constructor (private store : Store<{counter: number}>){
+    this.count$ = this.store.select('counter');
+    this.count$.subscribe();
+  }
+
+  // constructor(private counterService: CounterService) {}
 
   ngOnInit(): void {
-    this.counterServiceSub = this.counterService.counterChanged.subscribe(
-      (newVal) => (this.counter = newVal)
-    );
+  //   this.counterServiceSub = this.counterService.counterChanged.subscribe(
+  //     (newVal) => (this.counter = newVal)
+  //   );
   }
 
   ngOnDestroy(): void {
-    if (this.counterServiceSub) {
-      this.counterServiceSub.unsubscribe();
-    }
+  //   if (this.counterServiceSub) {
+  //     this.counterServiceSub.unsubscribe();
+  //   }
   }
 }
